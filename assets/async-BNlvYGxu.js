a3974d0899188c6fbe6bf3ad385428ebb3f9abce
douyi-FE <douyi-FE@users.noreply.github.com>
2025-07-04 03:41:18 +0000

    deploy: 112342de6ce2d27a5088a0771e2eda8a757d3cd5

--- assets/async-BNlvYGxu.js
+++
@@ -1,1 +0,0 @@
-import{m as n,c as s,o as c,a as o}from"./index-2aoRsBfM.js";const r={};function t(a,e){return c(),s("div",null,e[0]||(e[0]=[o("h1",null,"Async",-1)]))}const f=n(r,[["render",t]]);export{f as default};
