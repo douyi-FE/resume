112342de6ce2d27a5088a0771e2eda8a757d3cd5
Dou Yi <wangjie0806@qq.com>
2025-07-04 11:40:41 +0800

    修改router

--- apps/pc/src/router.ts
+++ apps/pc/src/router.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-    history: createWebHistory(process.env.VITE_BASE || '/'),
+    history: createWebHistory('/resume/'),
     routes: [
         { path: '/', component: () => import('@/views/home/home.vue') },
         { path: '/virtual-scroll', component: () => import('@/views/virtual-scroll/index.vue') },
